aa4b22722c0eecad81a3abac253f189ec90d02d4
Zerounary <850657663@qq.com>
2022-04-26 10:25:57 +0800

    判断程序端口是否存在，存在就干掉

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
-kill -9 $(lsof -t -i:9000)
+# kill server pid if exists
+if test -n "$(pidof echo-demo)"; then
+  echo "Killing server" 
+  kill -9 $(pidof echo-demo)
+fi
 # 避免nohup命令不终止
+echo "restart server"
 nohup /home/app/./echo-demo >/dev/null 2>&1 &
+echo "restart server done"
