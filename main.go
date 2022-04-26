cc19c3a7517cdf490843ca1b21a1d7c2ffeb725b
Zerounary <850657663@qq.com>
2022-04-26 12:41:23 +0800

    test save pid

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Echo Hello! use app variable")
+		return c.String(http.StatusOK, "Echo Hello! save pid as file")
 	})
 	e.Logger.Fatal(e.Start(":9000"))
 }
